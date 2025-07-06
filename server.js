95ec6984c64db9ca6529e04f525b8d63640e0707
SELYA DHARSNEE M <selyadharsneemuthusamy@gmail.com>
2025-07-06 14:18:57 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  if (extname === '.css') {
+    contentType = 'text/css';
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000');
+});
